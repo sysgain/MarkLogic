6888470d929fa9e704813247c7078d982b2a4df2
PradeepTS <pradeepts@sysgain.com>
2016-11-17 15:13:09 +0530

    Update create-forest-latest.sh
    updated the code to work in all vms

--- scripts/create-forest-latest.sh
+++ scripts/create-forest-latest.sh
@@ -2,21 +2,21 @@
 #	File         : forest-create.sh
 #	Description  : This script will create a forest in the local host
 #	Author	     : T S Pradeep Kumar
-#	Date		     : 11/15/2016
-# Version		   : V1.0
-# Usage        : sh filename.sh <localhost> <user-name> <password> <forest-name> <database-name>
+#	Date		 : 11/15/2016
+# Version		 : V1.0
 ######################################################################################################
 
 #!/bin/bash
 
-hostname=$1
-username=$2
-password=$3
-forestname=$4
-databasename=$5
+username=$1
+password=$2
+forestname=$3
+databasename=$4
+localhostname=`hostname`
+
 MLHOSTNAME=`curl --anyauth --user $username:$password -X GET \
--i -H "Content-type: application/json" http://localhost:8002/manage/v2/hosts | grep "<nameref>" | tail -2 | sed s/\<nameref\>// | sed s/\<.nameref\>// | tr -s ' ' '#' | sed s/#//`
+-i -H "Content-type: application/json" http://localhost:8002/manage/v2/hosts | grep "<nameref>" | grep "$localhostname" | sed s/\<nameref\>// | sed s/\<.nameref\>// | tr -s ' ' '#' | sed s/#//`
 
 curl --anyauth --user $username:$password -X POST \
 -d '{"forest-name":"'"$forestname"'", "host": "'"$MLHOSTNAME"'", "database": "'"$databasename"'"}' \
--i -H "Content-type: application/json" http://$hostname:8002/manage/v2/forests
+-i -H "Content-type: application/json" http://localhost:8002/manage/v2/forests
