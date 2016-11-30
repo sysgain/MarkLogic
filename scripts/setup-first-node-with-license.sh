b60de589cc0a5dd6070988aba0ca1e49a33602bb
PradeepTS <pradeepts@sysgain.com>
2016-11-30 14:47:53 +0530

    Update setup-first-node-with-license.sh
    latest file

--- scripts/setup-first-node-with-license.sh
+++ scripts/setup-first-node-with-license.sh
@@ -1,6 +1,7 @@
 ######################################################################################################
 #	File         :setup-first-node.sh
 #	Description  : This script will setup the head/first node in the cluster
+# Usage        : sh filename.sh hostname user password auth_mode n_retry retry_interval licencekey licensee
 ######################################################################################################
 
 #!/bin/bash
@@ -12,27 +13,21 @@ AUTH_MODE=$4
 SEC_REALM=$5
 N_RETRY=$6
 RETRY_INTERVAL=$7
-LICENSEKEY=$8
-LICENSEE=$9
-
-#BOOTSTRAP_HOST="localhost"
-#USER="sysgain1"
-#PASS="Sysga1n4205!"
-#AUTH_MODE="anyauth"
-#SEC_REALM="public"
-#N_RETRY=5
-#RETRY_INTERVAL=10
+LOGFILEDIR=$8
+LICENSEKEY=$9
+LICENSEE=${10}
 
 #Log file to record all the activities
 NOW=$(date +"%Y-%m-%d")
-mkdir /home/tspadmin/logfiles
+mkdir /home/$LOGFILEDIR/logfiles
+
 #############################################################
 # restart_check(hostname, baseline_timestamp, caller_lineno)
 #
 # Use the timestamp service to detect a server restart, given a
 # a baseline timestamp. 
 # Returns 0 if restart is detected, exits with an error if not.
-#
+##############################################################
 function restart_check {
   LAST_START=`$AUTH_CURL "http://$1:8001/admin/v1/timestamp"`
   for i in `seq 1 ${N_RETRY}`; do
@@ -43,7 +38,7 @@ function restart_check {
       return 0
     fi
   done
-  echo "ERROR: Line $3: Failed to restart $1" >> /home/tspadmin/logfiles/log-setup-first-node-$NOW.log
+  echo "ERROR: Line $3: Failed to restart $1" >> /home/$LOGFILEDIR/logfiles/log-setup-first-node-$NOW.log
   exit 1
 }
 
@@ -61,7 +56,7 @@ AUTH_CURL="${CURL} --${AUTH_MODE} --user ${USER}:${PASS}"
 ###################################################################
 
 # (1) Initialize the server
-echo "Initializing $BOOTSTRAP_HOST..." >> /home/tspadmin/logfiles/log-setup-first-node-$NOW.log
+echo "Initializing $BOOTSTRAP_HOST..." >> /home/$LOGFILEDIR/logfiles/log-setup-first-node-$NOW.log
 #$CURL -X POST -d "" http://${BOOTSTRAP_HOST}:8001/admin/v1/init
 #sleep 10
 
@@ -69,8 +64,9 @@ $CURL --anyauth -i -X POST \
    -H "Content-type:application/x-www-form-urlencoded" \
    --data-urlencode "license-key=${LICENSEKEY}" \
    --data-urlencode "licensee=${LICENSEE}" \
-   http://localhost:8001/admin/v1/init
+   -d ""  http://${BOOTSTRAP_HOST}:8001/admin/v1/init
 
+sleep 15
 
 TIMESTAMP=`$CURL -X POST \
    -H "Content-type: application/x-www-form-urlencoded" \
@@ -80,12 +76,12 @@ TIMESTAMP=`$CURL -X POST \
    | grep "last-startup" \
    | sed 's%^.*<last-startup.*>\(.*\)</last-startup>.*$%\1%'`
 if [ "$TIMESTAMP" == "" ]; then
-  echo "ERROR: Failed to get instance-admin timestamp." >&2 >> /home/tspadmin/logfiles/log-setup-first-node-$NOW.log
+  echo "ERROR: Failed to get instance-admin timestamp." >&2 >> /home/$LOGFILEDIR/logfiles/log-setup-first-node-$NOW.log
   exit 1
 fi
 
 # Test for successful restart
 restart_check $BOOTSTRAP_HOST $TIMESTAMP $LINENO
 
-echo "Initialization complete for $BOOTSTRAP_HOST..." >> /home/tspadmin/logfiles/log-setup-first-node-$NOW.log
+echo "Initialization complete for $BOOTSTRAP_HOST..." >> /home/$LOGFILEDIR/logfiles/log-setup-first-node-$NOW.log
 exit 0
